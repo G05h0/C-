f403271ee4e04fa97220ff905d3905814c4f99c4
Georgi Genov <123077056+G05h0@users.noreply.github.com>
2024-03-03 01:47:10 +0200

    Add files via upload

---
+++ 02. Data Types and Variables/04. Integer Operations/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int forthNumber = int.Parse(Console.ReadLine());
+
+int firstOperation = firstNumber + secondNumber;
+int secondOperation = firstOperation / thirdNumber;
+int thirdOperation = secondOperation * forthNumber;
+
+Console.WriteLine(thirdOperation);
