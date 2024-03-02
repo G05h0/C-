ff226673dd86eb4471acbf2b79819229c7cb209f
Georgi Genov <123077056+G05h0@users.noreply.github.com>
2024-03-02 22:20:15 +0200

    Add files via upload

---
+++ 01. Basic Syntax/01.Student Information/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Basic_Syntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            float grade = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {grade:f2}");
+        }
+    }
+}
---
+++ 01. Basic Syntax/08. Division/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int age = int.Parse(Console.ReadLine());
+
+if (age % 10 == 0 && age % 2 == 0)
+{
+    Console.WriteLine("The number is divisible by 10");
+}
+else if (age % 7 == 0)
+{
+    Console.WriteLine("The number is divisible by 7");
+}
+else if (age % 6 == 0)
+{
+    Console.WriteLine("The number is divisible by 6");
+}
+else if (age % 3 == 0)
+{
+    Console.WriteLine("The number is divisible by 3");
+}
+else if (age % 2 == 0)
+{
+    Console.WriteLine("The number is divisible by 2");
+}
+else
+{
+    Console.WriteLine("Not divisible");
+}
